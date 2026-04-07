34da7398becf966fa9e7366a9a273cd5ab6b3583
Alex Wills <alexwills115@gmail.com>
2026-04-06 23:40:02 -0500

    font, bg color, store, minor changes

--- MarketplaceOfIdeas/src/components/navbar.jsx
+++ MarketplaceOfIdeas/src/components/navbar.jsx
@@ -4,8 +4,9 @@ export default function NavBar() {
     
     return (
         <div className="my-2 py-2 border rounded-md border-purple-400 justify-center">
-           <Link className="mx-4"to="/">RECESS!!</Link>
-           <Link className="mx-4"to="/BigIdea">Bless the Big Idea</Link>
+           <Link className="mx-4"to="/">ABSCOND!!</Link>
+           <Link className="mx-4 btn"to="/BigIdea">Bless the Big Idea</Link>
+           <Link className="mx-4"to="/ShopIdeas">Shop Ideas</Link>
         </div>
     )
 }
---
+++ MarketplaceOfIdeas/src/pages/ShopIdeas.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function ShopIdeas() {
+
+    return (
+        <div>
+            <h1>Browse our Idealist Wares.</h1>
+            <p>shopshopshopshopshopshopshopshopshopshopshopshopshopshosphopshopshosphshopshosphpsh</p>
+        </div>
+    )
+}
+
