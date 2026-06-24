ef8b8627b295f252d8deb3ebc658b9be315e372e
Alex Wills <alexwills115@gmail.com>
2026-06-23 17:42:58 -0700

    major major changes. huge

--- MarketplaceOfIdeas/src/components/shopcard.jsx
+++ MarketplaceOfIdeas/src/components/shopcard.jsx
@@ -3,15 +3,9 @@ import kgb from '../assets/images/kgbimpressed.jpeg'
 export default function ShopCard({idea}) {
     const {title, image, desc, price} = idea
 
-<<<<<<< HEAD
    return(
         <div className='my-7 justify-center flex-col border rounded-md border-purple-600 text-white p-2'>
             <p className='leading-10 font-bold text-3xl'>{title}</p>
-=======
-    return(
-        <div className='my-7 mx-7 justify-center flex-col border rounded-md border-purple-600 text-white p-2'>
-            <p className='leading-10 font-bold text-3xl'>eat yo mattress</p>
->>>>>>> b944a0abb3cdb46f43a4e9dfc6c5318ef0448936
             <div>
                 <img className='mx-auto'width={200}src={image} alt="" />
             </div>
