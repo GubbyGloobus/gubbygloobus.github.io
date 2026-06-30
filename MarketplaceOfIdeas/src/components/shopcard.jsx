c8529ecb654f5c5e38da0924cd310a60b3255617
Alex Wills <alexwills115@gmail.com>
2026-06-30 15:10:17 -0700

    restocked the shop ideas

--- MarketplaceOfIdeas/src/components/shopcard.jsx
+++ MarketplaceOfIdeas/src/components/shopcard.jsx
@@ -4,14 +4,16 @@ export default function ShopCard({idea}) {
     const {title, image, desc, price} = idea
 
    return(
-        <div className='my-7 justify-center flex-col border rounded-md border-purple-600 text-white p-2'>
+        <div className='my-2 justify-center border rounded-md border-purple-600 text-white p-2 flex flex-col h-full'>
             <p className='leading-10 font-bold text-3xl'>{title}</p>
             <div>
                 <img className='mx-auto'width={200}src={image} alt="" />
             </div>
             <p>{desc}
             </p>
-            <p>{price}</p>
+            <div className= 'mt-auto text-green-600 font-bold text-green drop-shadow-[0_0_12px_rgba(255,255,255,0.9)]'>
+                <p>{price}</p>
+            </div>
         </div>
     )
 }
--- MarketplaceOfIdeas/src/pages/ShopIdeas.jsx
+++ MarketplaceOfIdeas/src/pages/ShopIdeas.jsx
@@ -6,9 +6,9 @@ export default function ShopIdeas() {
     const ideas = [
         {
             //STARTS AT ITEM #0//
-            title:'Steal USA\'s Secret File',
+            title:'"Steal" USA\'s The Secret File',
             image: 'src/assets/images/kgbimpressed.jpeg',
-            desc: 'you can get rich by selling secret from file',
+            desc: 'you can get rich by selling secret "from" file',
             price: '$400.93'
         },
 
@@ -20,23 +20,37 @@ export default function ShopIdeas() {
 
         },
 
-        {title: 'john the smith',
-            image: 'src/assets/images/homunculus.jpg',
-            desc: 'breden wont SHUT UP!!!!!!!!!!!!!!',
+        {title: 'Japanese Proverb',
+            image: 'src/assets/images/japaneseproverb.jpg',
+            desc: 'train to become a samurai and gain Intelligence Points with your peers. Proverbial goddess. The lifekind. Perchance.',
+            price: '¥200',
+        },
+
+        {title: 'Smel Bad',
+            image: 'src/assets/images/smel.jpg',
+            desc: 'create an aura to deter your annoying coworkers #stinkayyyy',
             price: 'priceless.',
         },
 
-        {title: 'john the smith',
-            image: 'src/assets/images/homunculus.jpg',
-            desc: 'breden wont SHUT UP!!!!!!!!!!!!!!',
-            price: 'priceless.',
+        {title: 'Run a Fade',
+            image: 'src/assets/images/fade.jpg',
+            desc: 'something annoying you? get rid of it yourself! self-sufficient, and classic.',
+            price: '$5,000 fine + up to 2 yrs prison',
+        },
+
+        {title: 'Get Your Fade Ran',
+            image: 'src/assets/images/punch.jpg',
+            desc: 'Provoke the strongest person you know. YEEEEEEEEOWWCH!! But in a character building way.',
+            price: 'hopital',
         },
     ]
 
     return (
         <div className="mx-5">
             <h1>Browse our Idealist Wares.</h1>
-            <p>shopshopshopshopshopshopshopshop  we think one new idea every wednesday.  hopshosphopshopshosphshopshosphpshop</p>
+            <p>shopshopshop  we think one new idea every wednesday. buy the idea, and make it happen yourself. hopshosphopshop<br />
+               <i>CLAIMER: if you use an idea without buying it first i will sue you big time</i> 
+            </p>
             <div className="grid grid-cols-3 justify-center gap-x-30 gap-y-5">
                 {
                     ideas.map((idea) => (
