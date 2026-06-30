06fabd9f2e9618a0c0ca4fe937aeb0b7a2f16d77
Alex Wills <alexwills115@gmail.com>
2026-06-30 16:37:42 -0700

    added shake text function

--- MarketplaceOfIdeas/src/pages/Support.jsx
+++ MarketplaceOfIdeas/src/pages/Support.jsx
@@ -5,8 +5,8 @@ export default function Support() {
     <div className="min-h-screen flex items-center justify-center bg-gray-900 px-4">
       <Link
         to="/ShopIdeas"
-        className="px-8 py-6 text-2xl font-bold rounded-xl bg-purple-600 text-white shadow-lg hover:bg-purple-700 transition duration-200">
-        Go to Shop Ideas
+        className="shake px-20 py-14 text-2xl font-bold rounded-xl bg-purple-600 text-white shadow-lg hover:bg-purple-700 transition duration-200">
+        THE ONLY HELP YOU NEED IS SHOPPING!!!!!!!!!!! GO!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       </Link>
     </div>
   );
